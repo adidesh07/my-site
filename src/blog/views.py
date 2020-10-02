f74255d3f3fef00277e0b35077f272cc3b23175b
adidesh07 <67908057+adidesh07@users.noreply.github.com>
2020-10-03 03:26:30 +0530

    Home page update

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .forms import CreateBlogPostForm
+from .forms import CreateBlogPostForm, UpdateBlogPostForm
 from account.models import Account
 from .models import BlogPost
 # Create your views here.
@@ -14,10 +14,11 @@ def create_blog_view(request):
     form = CreateBlogPostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         obj = form.save(commit=False)
-        author = Account.objects.filter(email=user.email).first()
+        author = Account.objects.filter(username=user.username).first()
         obj.author = author
         obj.save()
         form = CreateBlogPostForm()
+        return redirect('home')
 
     context['form'] = form
 
@@ -30,3 +31,47 @@ def detail_blog_view(request, slug):
     context['blog_post'] = blog_post
 
     return render(request, 'blog/detail_blog.html', context)
+
+
+def edit_blog_view(request, slug):
+    context = {}
+
+    user = request.user
+    if not user.is_authenticated:
+        return redirect('must_authenticate')
+
+    blog_post = get_object_or_404(BlogPost, slug=slug)
+    if request.POST:
+        form = UpdateBlogPostForm(request.POST or None, request.FILES or None, instance=blog_post)
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.save()
+            context['success_message'] = 'Updated Successfully!'
+            blog_post = obj
+
+    form = UpdateBlogPostForm(
+        initial={
+            'title': blog_post.title,
+            'body': blog_post.body,
+            'image': blog_post.image
+        }
+    )
+    context['form'] = form
+    return render(request, 'blog/edit_blog.html', context)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/personal/views.py
+++ src/personal/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from account.models import Account
+from blog.models import BlogPost
+from operator import attrgetter
 # Create your views here.
 
 def home_screen_view(request):
-    context = {
-        'accounts': Account.objects.all()
-    }
+    context = {}
+    blog_posts = sorted(BlogPost.objects.all(), key=attrgetter('date_updated'), reverse=True)
+    context['blog_posts'] = blog_posts
     return render(request,'personal/home.html',context)
