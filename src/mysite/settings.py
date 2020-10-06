1f12372da75dcf89ef550cab1a18421c4c13e9ae
adidesh07 <67908057+adidesh07@users.noreply.github.com>
2020-10-06 18:59:38 +0530

    Added REST APIs
    Created REST APIs for all the major functionalities

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -48,6 +48,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -77,6 +79,15 @@ TEMPLATES = [
         },
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
 
 AUTH_USER_MODEL = 'account.Account'
 
--- src/mysite/urls.py
+++ src/mysite/urls.py
@@ -35,6 +35,12 @@ urlpatterns = [
     path('account/', account_view, name='account'),
     path('must_authenticate/', must_authenticate, name='must_authenticate'),
 
+
+    # REST FRAMEWORK URLS
+    path('api/blog/', include('blog.api.urls','blog_api')),
+    path('api/account/', include('account.api.urls','account_api')),
+
+
     # Password reset links (ref: https://github.com/django/django/blob/master/django/contrib/auth/views.py)
     path('password_change/done/',
          auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'),
